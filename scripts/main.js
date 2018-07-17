f90bbd6047cb9cd5f1092383899b8cc61ff9a919
Lilian <moulan_l16@myges.fr>
2018-07-17 17:48:33 +0200

    presentation

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ window.addEventListener('scroll', function() {
 
 function togleh1() {
   sections = document.querySelectorAll("body > section");
+  nav = document.getElementById('nav');
   [].forEach.call(sections, function(section) {
     if ((section.offsetTop - window.scrollY) < 300 && (section.offsetTop + section.offsetHeight - window.scrollY) > -300) {
       if (!section.classList.contains("active")) {
@@ -11,6 +12,8 @@ function togleh1() {
           section.classList.remove('active');
         })
         section.classList.add('active');
+        nav.classList.remove('color-accueil', 'color-pourquoi', 'color-quoi', 'color-comment', 'color-qui');
+        nav.classList.add('color-' + section.id);
       }
     }
   });
