ff21207107532680069d5460e4bdb5cd53d790f5
Lilian Moulanier <lilian.moulanier@outlook.fr>
2018-07-17 12:27:40 +0200

    pager

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
 window.addEventListener('scroll', function() {
-  console.log('scroll');
   togleh1();
 });
 
 function togleh1() {
   sections = document.querySelectorAll("body > section");
   [].forEach.call(sections, function(section) {
-    console.log(section.offsetTop - window.scrollY);
-    if
+    if ((section.offsetTop - window.scrollY) < 300 && (section.offsetTop + section.offsetHeight - window.scrollY) > -300) {
+      if (!section.classList.contains("active")) {
+        [].forEach.call(sections, function(section) {
+          section.classList.remove('active');
+        })
+        section.classList.add('active');
+      }
+    }
   });
 }
+
+togleh1();
